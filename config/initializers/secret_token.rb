4c02bb38d8888eb0097b2065874f28a49909a652
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-09 12:49:59 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Celestin::Application.config.secret_token = 'b983d4f16058b47556c9cbfdd2e37941608809321fcec0e84db493d1b39ea4ef4524e3abb8b39869e87ef1534b19ebdb2587d06edaf5366b9bd21774655f39bd'
